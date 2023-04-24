45ae6156b36343cf172aa07c8dc0890a8aa209ac
JTFlavier <javyflavier@gmail.com>
2023-04-24 21:18:16 +0800

    Updates

---
+++ precache-manifest.3f59c2b5dba840659093f550353bac14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b3554a41ce0b755131",
+    "url": "/book-reco-app/static/css/main.67f3d4a1.chunk.css"
+  },
+  {
+    "revision": "12b3554a41ce0b755131",
+    "url": "/book-reco-app/static/js/main.a7e77fbd.chunk.js"
+  },
+  {
+    "revision": "3e06e5861cd5213aff55",
+    "url": "/book-reco-app/static/js/runtime~main.12953be0.js"
+  },
+  {
+    "revision": "615cc0e65cc761a7a898",
+    "url": "/book-reco-app/static/js/2.948f82ab.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/book-reco-app/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "753426fd0894f4a182b182b5f8668e6c",
+    "url": "/book-reco-app/index.html"
+  }
+];
